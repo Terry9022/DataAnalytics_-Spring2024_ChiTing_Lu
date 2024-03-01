f5940af0229424f1d477f034422361199cddf305
Chi-Ting Lu (Terry) <47840075+Terry9022@users.noreply.github.com>
2024-02-29 23:58:25 -0500

    Delete group2 directory

--- group2/lab1_mosaic.R
+++
@@ -1,4 +0,0 @@
-data(HairEyeColor)
-mosaicplot(HairEyeColor)
-margin.table(HairEyeColor,3)
-margin.table(HairEyeColor,c(1,3))
--- group2/lab1_spm.R
+++
@@ -1,5 +0,0 @@
-install.packages("car")
-require(car)
-scatterplotMatrix(iris)
-# and
-scatterplotMatrix(swiss)
